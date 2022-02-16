651fa55bd552424bd1decbe3411f56d67a3874d9
M-Kia <smhkh2001@gmail.com>
2022-02-16 20:20:17 +0330

    .

--- src/apis/API.js
+++ src/apis/API.js
@@ -8,16 +8,18 @@ const getWeatherAndForecast = (coordinates, setResult) => {
       `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${KEY}&units=metric`
     )
     .then((res) => {
+      console.log("getWeatherAndForecast", res);
       setResult(res.data);
     });
-};
-
-const getCityCoordinates = async (cityName, setResultCity) => {
-  let x = await axios
+  };
+  
+  const getCityCoordinates = async (cityName, setResultCity) => {
+    let x = await axios
     .get(
       `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${KEY}`
-    )
-    .then((res) => {
+      )
+      .then((res) => {
+      console.log("getCityCoordinates", res);
       return res.data;
     });
   setResultCity(x);
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,50 +1,71 @@
-import React from "react";
-import Moment from "react-moment";
-import "moment-timezone";
+import React, { useEffect, useState } from "react";
+import useInterval from "./TimeInterval";
 
 import "./WeatherInfo.css";
 import Icon from "./Icon";
 import Day from "./Day";
 
 const WeatherInfo = ({ result, timeOfCountry, timezone }) => {
-  if (result != null && timeOfCountry != null && timezone != null) {
-    // const date = timeOfCountry.slice(0, 10).replaceAll("-", "/");
-    // const time = timeOfCountry.slice(11, 16);
-    return (
-      <div className="weather">
-        <Icon result={result.current} />
+  const [time, setTime] = useState(null);
 
-        <div className="weather_">
-          <div className="weather_temp">
-            <div style={{ color: "#D52727" }}>{result.current.temp}°C</div>
-            Real Feel : {result.current.feels_like}
-            <br />
-            {result.current.weather[0].description}
-          </div>
+  useInterval(
+    () => {
+      setTime((t) => new Date(t.getTime() + 1000));
+    },
+    time ? 1000 : null
+  );
+
+  useEffect(() => {
+    if (result != null && timeOfCountry != null && timezone != null) {
+      let date = new Date(timeOfCountry * 1000);
+      setTime(
+        new Date(
+          date.toLocaleString("en-US", {
+            timeZone: timezone,
+          })
+        )
+      );
+    }
+  }, [result, timeOfCountry, timezone]);
+
+  if (result == null && timeOfCountry == null && timezone == null) {
+    return null;
+  }
+  return (
+    <div className="weather">
+      <Icon result={result.current} />
+
+      <div className="weather_">
+        <div className="weather_temp">
+          <div style={{ color: "#D52727" }}>{result.current.temp}°C</div>
+          Real Feel : {result.current.feels_like}
           <br />
-          <div className="weather_other">
-            Humidity : {result.current.humidity}%
-            <br />
-            Wind :{" "}
-            {(Math.round(result.current.wind_speed * 3.6 * 100) / 100).toFixed(
-              2
-            )}{" "}
-            km/h
-          </div>
+          {result.current.weather[0].description}
         </div>
-        <div className="weather_description">
-          <Day index={-1} />
-          <Moment unix tz={timezone}>
-            {timeOfCountry}
-          </Moment>
-          {/* <br />
-          {time}
-          <br /> */}
+        <br />
+        <div className="weather_other">
+          Humidity : {result.current.humidity}%
+          <br />
+          Wind :
+          {(Math.round(result.current.wind_speed * 3.6 * 100) / 100).toFixed(2)}
+          km/h
         </div>
       </div>
-    );
-  }
-  return null;
+      <div className="weather_description">
+        <Day index={-1} />
+        {/* <Moment format="hh:mm:ss" interval={1000} date={time} duration={futureTime} /> */}
+        {time
+          ? `${time.getHours() < 10 ? "0" : ""}${time.getHours()}:${
+              time.getMinutes() < 10 ? "0" : ""
+            }${time.getMinutes()}`
+          : ""}
+        {/* {dd} */}
+        {/* <br />
+          {time}
+          <br /> */}
+      </div>
+    </div>
+  );
 };
 
 export default WeatherInfo;
